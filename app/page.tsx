f48bd4087f7c63ff53b9264fc732f61d6d7bb485
Print Box <printboxmadagascar@gmail.com>
2025-02-20 00:09:48 +0300

    Message descriptif de votre commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import OneAnimation from "./_components/1-animation";
-import TwoAnimation from "./_components/2-animation";
-import ThreeAnimation from "./_components/3-animation";
+import NavbarMenu from "./_components/navabar-menu";
+
 
 export default function Home() {
   return (
     <>
       <div className="w-full h-auto flex justify-center items-center py-4">
         <h1 className="text-3xl md:text-4xl font-bold bg-blue-400 uppercase">
-          motion for animation
-        </h1>
+navigation        </h1>
       </div>
 
-      <OneAnimation />
-      <TwoAnimation />
-      <ThreeAnimation />
+   
+      <NavbarMenu />
     </>
   );
 }
